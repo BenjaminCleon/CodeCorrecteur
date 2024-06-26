acb495781823d25b613ee7b5cd60cf5b91b0b8b0
Horusrt <damien.segui31@gmail.com>
2024-06-26 09:37:34 +0200

    Added: TGraph file

---
+++ TGraph.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+public class TGraph {
+    private int[][] left = null;
+    private int[][] right = null;
+
+    private int n_c = 0, w_c = 0, n_r = 0, w_r = 0;
+
+    public TGraph (Matrix H, int wc, int wr) {
+
+    }
+}
